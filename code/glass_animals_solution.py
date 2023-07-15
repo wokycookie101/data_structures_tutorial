58c631de66d9ab4ff829e276c31acf6602f11a9a
Ariana Jerome <reannja@gmail.com>
2023-07-15 16:54:06 -0600

    Added adjustments

--- code/glass_animals_solution.py
+++ code/glass_animals_solution.py
@@ -1,9 +1,12 @@
-
 # Glass Animal Collection 
 sea_creatures = {"Dolphin", "Sea Horse", "Gold Fish", "Shrimp", "Shark", "Cat", "Sea Turtle", "Orca"}
 land_creatures = {"Fox", "Whale",  "Elephant", "Monkey", "Dog", "Raccoon", "Rhino", "Spider"}
 
-# TO DO: Find the odd ones out and put them in the right lists
+sea_creatures.remove("Cat")
+land_creatures.remove("Whale")
+
+sea_creatures.add("Whale")
+land_creatures.add("Cat")
 
 print(sea_creatures)
 print(land_creatures)
